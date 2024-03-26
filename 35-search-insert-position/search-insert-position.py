92af0a09229dccdf1e8e15c606b88d0e0e2cf67b
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-25 23:28:50 -0700

    Time: 37 ms (98.83%) | Memory: 17.3 MB (85.28%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,19 +1,29 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1 
+        # left = 0
+        # right = len(nums) - 1 
         
 
-        while left <= right:
-            mid = (right + left) // 2
-            print ("mid: ", mid, nums[mid])
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target: 
-                left = mid + 1
-            else:
-                right = mid - 1
+        # while left <= right:
+        #     mid = (right + left) // 2
+        #     print ("mid: ", mid, nums[mid])
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target: 
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
 
-            print ("leftt: ", left,"===", right)
+        #     print ("leftt: ", left,"===", right)
 
-        return left
+        # return left
+
+        # another way to do it using function lib in python
+        if target in nums:
+            return nums.index(target)
+        
+        # then add the num add the end of the list 
+        # sort it and find the index again
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
