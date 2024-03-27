899122021fc7de74ea8b1f7fe415d17a9a054000
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-26 17:10:28 -0700

    Time: 0 ms (100.00%) | Memory: 7.5 MB (52.20%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+      const unsigned int range = 1<<32;
+      long long reversedNum = 0; 
+      bool checkNeg = false; 
+      if (x < 0 ) {
+        checkNeg = true;
+        x = abs(x); 
+      } 
+
+      if (abs(x) > range)
+            return 0;
+
+      while (x > 0) {
+        if (reversedNum * 10 > range)
+            return 0;
+        reversedNum = reversedNum * 10 + (x % 10);
+        x /= 10; 
+       
+      }
+        cout << "\nrange: " << range;
+      if (reversedNum < range)
+        return checkNeg?-reversedNum:reversedNum;  
+      return 0;
+    }
+};
