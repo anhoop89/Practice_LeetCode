f26bb6441d0ecaacaa45757a9ab5d3a3b9fd0809
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-28 23:03:59 -0700

    Time: 21 ms (27.69%) | Memory: 18.3 MB (27.16%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,30 +3,45 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
     
         // result will collect all the combination results
+        
+        // need a vector to store all the combination
         vector<vector<int>> result; 
-       
-        findCombination(result, {} ,candidates,target, 0);
+
+        //  because there's more than one result that combine to be equal to the target. 
+        //  I need a helper function named findCombination to support this
+        //  findCombination(result, single vector to store a current result, candidates, target, position)
+        findCombination(result, {}, candidates, target, 0); 
         
         return result;
     }
 
-    void findCombination(vector<vector<int>>& result, vector<int> curr, vector<int>& candidates, int target, int pos) {
-        if (target == 0) {
-            result.push_back(curr);
+    //  recursion function to find a list of the chosen numbers. 
+    void findCombination(vector<vector<int>> & result, vector<int> current, vector<int>& candidates, int currTarget, int pos) {
+        // base cases - 2 bases case
+        // 1st base case
+        if (currTarget < 0)
             return;
-        }
+        // 2nd base case
+        // current target == 0 means that sum of the chosen numbers - currentTarget == 0 ( matched!) adding them into the result
+        if (currTarget == 0) {
+            result.push_back(current);
+            return;
+        }   
 
-        if (target < 0)
-            return;
-        
+        // need a loop to check all the candiates number that potentially matches the sum of the chosen number in the current arr
         while (pos < candidates.size()) {
-            curr.push_back(candidates[pos]);
-            findCombination(result, curr, candidates, target - candidates[pos], pos);
-            pos++;
-            curr.pop_back();
-        }
+            // add a number in candidates arr to the current arr for checking
+            current.push_back(candidates[pos]);
+            // recursive method to handle the target minus each number in candidates to find a matching combination.
+            findCombination(result, current, candidates, currTarget - candidates[pos], pos); 
 
+            //  if it's failed with the base case, it will return the previous stack. another is previous currTarget > 0 
+            pos++; 
+            //  remove the prevous chosen number because it doesn't qualify the sum for the currTarget
+            current.pop_back();
+        }
     }
+
 };
 
 // 
