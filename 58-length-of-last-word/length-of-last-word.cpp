bb77cab3b7c074e6a948000f1dfffd836f7e1ed0
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-30 23:55:12 -0700

    Time: 10 ms (47.86%) | Memory: 7.7 MB (20.33%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0;
      
         for (int i = s.length() - 1; i >= 0 ; --i) {
-             cout << "\n count COUNT : " << count << " --- " << s[i] << "   " << s.length() << endl;
+            cout << "\n count COUNT : " << count << " --- " << s[i] << "   " << s.length() << endl;
             if (s[i] == ' ') {
                 if (count != 0) 
                     return count; 
