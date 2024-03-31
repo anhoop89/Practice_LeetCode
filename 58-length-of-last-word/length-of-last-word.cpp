f1f6d1679831c0ac069c0cb67f72f4c73e9cb09e
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-30 23:53:28 -0700

    Time: 0 ms (100.00%) | Memory: 7.6 MB (87.93%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+     
+        for (int i = s.length() - 1; i >= 0 ; --i) {
+             cout << "\n count COUNT : " << count << " --- " << s[i] << "   " << s.length() << endl;
+            if (s[i] == ' ') {
+                if (count != 0) 
+                    return count; 
+            } else
+            count++;
+            cout << "\n count : " << count << " --- " << s[i] << endl;
+        }
+        return count;
+    }
+};
