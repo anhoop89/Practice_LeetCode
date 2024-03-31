44c7eab8d4b318b8b25b4989a0d4f700b40c7e62
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-03-31 02:10:28 -0700

    Time: 5 ms (25.99%) | Memory: 10.2 MB (55.00%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
-        vector<int> result;
+        // int carry = 1;
+        // vector<int> result;
 
-        for (int i = digits.size()-1; i >= 0; --i) {
-            int sum = digits[i] + carry; 
-            result.push_back(sum % 10);
-            // update the carry
-            carry = sum / 10;
-        }
+        // for (int i = digits.size()-1; i >= 0; --i) {
+        //     int sum = digits[i] + carry; 
+        //     result.push_back(sum % 10);
+        //     // update the carry
+        //     carry = sum / 10;
+        // }
 
-            // If there is still a carry after iterating through all digits
-        if (carry > 0) {
-            result.push_back(carry);
-        }
+        //     // If there is still a carry after iterating through all digits
+        // if (carry > 0) {
+        //     result.push_back(carry);
+        // }
        
-        reverse(result.begin(), result.end());
-        return result;
+        // reverse(result.begin(), result.end());
+        // return result;
+
+        // better way
+        for (int i = digits.size()-1; i >= 0; --i) {
+                if (digits[i]  < 9 ) {
+                    digits[i]++;
+                    return digits;
+                } else {
+                    digits[i] = 0;
+                }
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+
     }
 };
