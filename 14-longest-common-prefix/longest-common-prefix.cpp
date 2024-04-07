27a213535116c1c9d1ae049d9f47ba4c34d7d217
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-07 00:08:35 -0700

    Time: 0 ms (100.00%) | Memory: 10.8 MB (87.87%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -13,8 +13,12 @@ public:
 
         // Iterate over characters of the shortest string and compare with other strings
         int j = 0;
+
+        //check each element in a string
         while (j < minLen) {
             int k = 0;
+
+            //check each string
             while (k < strs.size()) {
                 if (strs[k][j] != str[j]) {
                     return str.substr(0, count);
