6a15e580fe20309364685d5134d8588df2d83a8b
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-06 23:32:24 -0700

    Time: 3 ms (70.79%) | Memory: 11 MB (59.66%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int minLen = 222;
+        int count = 0;
+        string str;
+        for (string & s : strs) {
+            if (s.length() < minLen ) {
+                minLen = s.length(); 
+                str = s; 
+            }
+        }
+
+        // Iterate over characters of the shortest string and compare with other strings
+        int j = 0;
+        while (j < minLen) {
+            int k = 0;
+            while (k < strs.size()) {
+                if (strs[k][j] != str[j]) {
+                    return str.substr(0, count);
+                }
+                k++;
+            }
+            count++;
+            j++;
+        }
+
+        return str.substr(0, count);
+    }
+};
