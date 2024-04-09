a6fd85456c918bc1e33ebf70e53a1c6e41ffca06
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-09 00:30:01 -0700

    Time: 0 ms (100.00%) | Memory: 7.2 MB (61.51%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -4,25 +4,23 @@ public:
         if (n == INT_MAX)
             return false;
         int sum = 0;
-        vector<int> visited;
-         return helper(n, sum, visited);
+       
+        return  helper(n, sum);
     }
 
-     bool helper(int n , int sum, vector<int>& visited) {
-    
+    bool helper(int n , int sum) {
         if (n == 1) 
             return true;
         
+        if (n == 4 )
+            return false;
         while (n  > 0) {
             int num = (n % 10);
             sum +=  num  * num;
             cout << "num: " << n%10 << " --- sum loves js: " << sum << endl;
             n /= 10; 
         }
-        
-        if (find(visited.begin(), visited.end(), sum) != visited.end())
-            return false; // Cycle detected, the number is not happy
-        visited.push_back(sum);
-        return  helper(sum, 0, visited);
+
+        return  helper(sum, 0);
     }
 };
