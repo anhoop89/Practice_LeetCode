492563f2b104432b7ded0024a324796e3f91bffd
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-10 00:53:17 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (46.66%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -20,3 +20,16 @@ public:
     }
   
 };
+
+/* noted:
+1      2         3        4         5    6 7 
+head   newhead
+
+2         -> 1     ---> 3 
+newhead      head     head->next->next
+
+head->next->next because head was at 1 at the beigning,
+so the updated head->next should be 3 then we have head->next->next. if null, return head 
+
+3   4 would be doing the same when passing the recursive function of swapPairs(head->next->next);
+*/
