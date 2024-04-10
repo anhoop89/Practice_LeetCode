97d89bfa903bec52f091dd3bcaef100a90a9eb45
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-09 23:16:55 -0700

    Time: 0 ms (100.00%) | Memory: 7.3 MB (25.94%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -3,9 +3,9 @@ public:
     bool isUgly(int n) {
       bool check = false;
       while ( n > 1 ) {
-        if ( n % 2 == 0) {n /= 2; check = true; }
-        else if ( n % 3 == 0) {n /= 3; check = true; }
-        else if ( n % 5 == 0) {n /= 5; check = true; }
+        if ( n % 2 == 0) n /= 2; 
+        else if ( n % 3 == 0) n /= 3; 
+        else if ( n % 5 == 0) n /= 5; 
         else return false;
         
         cout << "n:  " << n << endl;
