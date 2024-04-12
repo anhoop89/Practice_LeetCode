9119e3637a1d1f0cb11847d8e5f0cc6bb71d0044
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-12 11:44:12 -0700

    Time: 3 ms (28.44%) | Memory: 6.9 MB (97.58%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,14 +5,14 @@ public:
         // its own step
         if (n <= 2) return n; 
         // one step or two step
-        int one = 1;
-        int two = 2;
+        int previous = 1;
+        int current = 2;
 
         int count = 0; 
         for (int i = 3; i <= n; i++) {
-            count = one + two; // The number of ways to reach the current step is the sum of the ways to reach the previous two steps.
-            one = two; // Update the number of ways for the previous step (one step back).
-            two = count; // Update the number of ways for the current step.
+            count = previous + current; // The number of ways to reach the current step is the sum of the ways to reach the previous two steps.
+            previous = current; // Update the number of ways for the previous step (one step back).
+            current = count; // Update the number of ways for the current step.
         }
         return count;
     }
