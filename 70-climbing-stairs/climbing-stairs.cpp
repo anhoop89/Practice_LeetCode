5f21d0712be389468600d130d986b2dfe16368e7
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-12 11:38:10 -0700

    Time: 2 ms (47.17%) | Memory: 7 MB (85.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
+    // no recursion
     int climbStairs(int n) {
-        // Base cases
         if (n == 0 || n == 1) return 1;
-
-        // Initialize variables to store the number of ways to reach steps 0 and 1
-        int prev = 1; // Number of ways to reach step 0
-        int curr = 1; // Number of ways to reach step 1
-
-        // Iterate from step 2 to step n
-        for (int i = 2; i <= n; ++i) {
-            // Calculate the number of ways to reach step i by summing the number of ways to reach steps i-1 and i-2
+        int prev = 1;
+        int curr = 1;
+        for (int i = 1; i < n ; ++i) {
             int next = prev + curr;
-            // Update the previous and current variables for the next iteration
-            prev = curr;
+            prev = curr; 
             curr = next;
-        }
-
-        // Return the number of ways to reach step n
-        return curr;
+        } 
+        return curr; 
+        
     }
 };
-/*
-
-*/
