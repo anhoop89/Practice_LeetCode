145ac416263b96ab04123ae4183c97ec032d704f
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-15 10:33:26 -0700

    Time: 3 ms (31.31%) | Memory: 7.6 MB (6.12%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -23,9 +23,11 @@
 //     int findCombination(int num, map<int, int> & arr) {
 //         int ways = 0;
 //         if (arr.find(num) == arr.end()) {
-//             for (int i = 1; i < num ; i++) {
-//                 arr[i+1]= arr[i] * num; 
+//             for (int i = 1; i <= num ; i++) {
+//               //  arr[i+1]= arr[i] * num; 
+//                  ways = findCombination(i-1, arr) *
 //             }
+//              arr[num] = ways;
 //         }
 //         return arr[num];
 //     }
@@ -41,7 +43,34 @@
 // 3: left 0 * 2 ways. 
 // */
 
+// 1 way
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         // Store all the combination ways
+//         map<int, int> arr;
+//         arr[0] = 1; // Base case for factorial: 1 way to arrange 0 nodes
+//         arr[1] = 1; // Base case for factorial: 1 way to arrange 1 node
+        
+//         if (n == 0)
+//             return 0; 
+        
+//         return findCombination(n, arr);
+//     }
 
+//     int findCombination(int num, map<int, int> &arr) {
+//         if (arr.find(num) == arr.end()) {
+//             int ways = 0;
+//             for (int i = 1; i <= num; ++i) {
+//                 ways += findCombination(i - 1, arr) * findCombination(num - i, arr);
+//             }
+//             arr[num] = ways;
+//         }
+//         return arr[num];
+//     }
+// };
+
+//the other way
 class Solution {
 public:
     int numTrees(int n) {
@@ -50,20 +79,15 @@ public:
         arr[0] = 1; // Base case for factorial: 1 way to arrange 0 nodes
         arr[1] = 1; // Base case for factorial: 1 way to arrange 1 node
         
-        if (n == 0)
-            return 0; 
-        
-        return findCombination(n, arr);
-    }
-
-    int findCombination(int num, map<int, int> &arr) {
-        if (arr.find(num) == arr.end()) {
+        // Calculate combination ways iteratively
+        for (int i = 2; i <= n; ++i) {
             int ways = 0;
-            for (int i = 1; i <= num; ++i) {
-                ways += findCombination(i - 1, arr) * findCombination(num - i, arr);
+            for (int j = 1; j <= i; ++j) {
+                ways += arr[j - 1] * arr[i - j];
             }
-            arr[num] = ways;
+            arr[i] = ways;
         }
-        return arr[num];
+
+        return arr[n];
     }
 };
