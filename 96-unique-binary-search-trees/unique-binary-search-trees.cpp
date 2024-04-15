2ad150e2a063d40603fa0f2b1efd35225d26ca60
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-15 10:35:44 -0700

    Time: 3 ms (31.31%) | Memory: 7.9 MB (6.12%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -44,33 +44,6 @@
 // */
 
 // 1 way
-// class Solution {
-// public:
-//     int numTrees(int n) {
-//         // Store all the combination ways
-//         map<int, int> arr;
-//         arr[0] = 1; // Base case for factorial: 1 way to arrange 0 nodes
-//         arr[1] = 1; // Base case for factorial: 1 way to arrange 1 node
-        
-//         if (n == 0)
-//             return 0; 
-        
-//         return findCombination(n, arr);
-//     }
-
-//     int findCombination(int num, map<int, int> &arr) {
-//         if (arr.find(num) == arr.end()) {
-//             int ways = 0;
-//             for (int i = 1; i <= num; ++i) {
-//                 ways += findCombination(i - 1, arr) * findCombination(num - i, arr);
-//             }
-//             arr[num] = ways;
-//         }
-//         return arr[num];
-//     }
-// };
-
-//the other way
 class Solution {
 public:
     int numTrees(int n) {
@@ -79,15 +52,42 @@ public:
         arr[0] = 1; // Base case for factorial: 1 way to arrange 0 nodes
         arr[1] = 1; // Base case for factorial: 1 way to arrange 1 node
         
-        // Calculate combination ways iteratively
-        for (int i = 2; i <= n; ++i) {
-            int ways = 0;
-            for (int j = 1; j <= i; ++j) {
-                ways += arr[j - 1] * arr[i - j];
-            }
-            arr[i] = ways;
-        }
+        if (n == 0)
+            return 0; 
+        
+        return findCombination(n, arr);
+    }
 
-        return arr[n];
+    int findCombination(int num, map<int, int> &arr) {
+        if (arr.find(num) == arr.end()) {
+            int ways = 0;
+            for (int i = 1; i <= num; ++i) {
+                ways += findCombination(i - 1, arr) * findCombination(num - i, arr);
+            }
+            arr[num] = ways;
+        }
+        return arr[num];
     }
 };
+
+//the other way
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         // Store all the combination ways
+//         map<int, int> arr;
+//         arr[0] = 1; // Base case for factorial: 1 way to arrange 0 nodes
+//         arr[1] = 1; // Base case for factorial: 1 way to arrange 1 node
+        
+//         // Calculate combination ways iteratively
+//         for (int i = 2; i <= n; ++i) {
+//             int ways = 0;
+//             for (int j = 1; j <= i; ++j) {
+//                 ways += arr[j - 1] * arr[i - j];
+//             }
+//             arr[i] = ways;
+//         }
+
+//         return arr[n];
+//     }
+// };
