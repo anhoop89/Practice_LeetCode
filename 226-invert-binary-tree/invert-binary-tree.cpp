997b39387432eb7c4ad0d503479ffe8678ba3b1e
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-18 22:56:12 -0700

    Time: 5 ms (27.28%) | Memory: 11.3 MB (84.42%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -17,10 +17,8 @@ public:
         TreeNode * temp = root->left;
         root->left = root->right;
         root->right = temp; 
-        root->left = invertTree(root->left);
-        root->right = invertTree(root->right);
+        invertTree(root->left);
+        invertTree(root->right);
         return root;                
     }
-
-
 };
