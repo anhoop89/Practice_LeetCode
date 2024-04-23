aa04cc6911a5c6700d231093c2d35c39c621350b
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-23 13:17:46 -0700

    Time: 17 ms (34.09%) | Memory: 18.8 MB (25.43%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -8,22 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         if (!head)
+//             return nullptr;
+//         if (head->val == val) {
+//             ListNode * toDelete = head;
+//             head = head->next;
+//             delete toDelete; 
+//             // If you remove the first node (the head of the list), 
+//             // and you don't use the result of the recursive call, the previous calls won't know that the head has changed. 
+
+//             //that's why i need a return here to update the head
+//             return  removeElements(head, val);
+//         }
+//         head-> next = removeElements(head->next, val);
+//         return head;
+//     }
+// };
+
+// optimize solution 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if (!head)
-            return nullptr;
-        if (head->val == val) {
-            ListNode * toDelete = head;
-            head = head->next;
-            delete toDelete; 
-            // If you remove the first node (the head of the list), 
-            // and you don't use the result of the recursive call, the previous calls won't know that the head has changed. 
+        // Start with a sentinel node.
+        ListNode* sentinel = new ListNode(0);
+        sentinel->next = head;
 
-            //that's why i need a return here to update the head
-            return  removeElements(head, val);
+        // Use two pointers: prev and curr.
+        ListNode* prev = sentinel;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                // Remove the current node.
+                prev->next = curr->next;
+                delete curr;  // Free memory of the node being removed.
+                curr = prev->next;  // Move curr to the next node.
+            } else {
+                // Move prev and curr one step forward when no deletion occurs.
+                prev = curr;
+                curr = curr->next;
+            }
         }
-        head-> next = removeElements(head->next, val);
-        return head;
+
+        // The new head of the list.
+        ListNode* newHead = sentinel->next;
+        delete sentinel;  // Clean up the sentinel node.
+        return newHead;
     }
 };
