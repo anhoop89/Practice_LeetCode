c3fac1a8b5360da136352256799cb2de2035b5e6
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-05-05 23:00:14 -0700

    Time: 52 ms (52.61%) | Memory: 48.7 MB (67.43%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -2,14 +2,20 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    let count = 0;
-    for (let i = 0; i < args.length ; i++) {
-        count++;
-    }
-    return count;
-};
+// var argumentsLength = function(...args) {
+//     let count = 0;
+//     for (let i = 0; i < args.length ; i++) {
+//         count++;
+//     }
+//     return count;
+// };
 
+var argumentsLength = function(...args) {
+    return args.length;
+};
+/*
+ wow i am so dumb, why i dont call args.length
+*/
 /**
  * argumentsLength(1, 2, 3); // 3
  */
