175a832fa7aa89b55c18558db544494eebccc0c3
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-05-29 10:12:37 -0700

    Time: 2058 ms (96.20%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,11 +1,11 @@
 SELECT
     v.customer_id,
-      COUNT(CASE WHEN t.transaction_id IS NULL THEN 1 END) AS count_no_trans
+    COUNT(v.visit_id) AS count_no_trans
 FROM
     Visits v
 LEFT JOIN
     Transactions t ON v.visit_id = t.visit_id
 WHERE
     t.transaction_id IS NULL
-group by 
+GROUP BY
     v.customer_id;
