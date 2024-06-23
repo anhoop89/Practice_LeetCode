b222e3aa6800040510fa946e97de66363a78924f
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-22 23:24:58 -0700

    Time: 140 ms (6.91%) | Memory: 73.2 MB (44.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,41 @@
 class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+
+// complexity: nlogn
+// space: O(1)
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
 
         
-        for (int i = 0; i < nums.size() - 1; ++i ) {
-            // if the pair is matching, return true that meant at least one pair
-            if (nums[i] == nums[i+1])
+//         for (int i = 0; i < nums.size() - 1; ++i ) {
+//             // if the pair is matching, return true that meant at least one pair
+//             if (nums[i] == nums[i+1])
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+// time: On
+// space: On 
+// hashMap
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // create a hasharr
+        unordered_map<int, int> storedNum; 
+
+        for (int i = 0; i < nums.size(); i++ ) {
+            cout << "nums[0] : " << nums[i] << endl;
+            if (storedNum.find(nums[i]) != storedNum.end())
                 return true;
+            else 
+                storedNum[nums[i]] = i;
         }
+
         return false;
     }
 };
+
+
+// solution: do hashSet => review must do hashSet method
