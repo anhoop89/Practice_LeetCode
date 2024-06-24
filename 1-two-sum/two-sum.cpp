2cf05ef757f8ddc24b3abd61b8cbda5841232336
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-24 11:48:18 -0700

    Time: 8 ms (78.17%) | Memory: 14 MB (38.65%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,76 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> storedArr; 
-        // initialize the map
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     unordered_map<int, int> storedArr; 
+    //     // initialize the map
   
-        for (int i = 0; i < nums.size(); i++) 
-        {
-            int findValue = target - nums[i];
-            cout << findValue << "   " << i << endl;
-            if (storedArr.find(findValue) != storedArr.end() ) 
-                return {i, storedArr[findValue]};
-            // if we don't find it , we should add the ele of nums to map
+    //     for (int i = 0; i < nums.size(); i++) 
+    //     {
+    //         int findValue = target - nums[i];
+    //         cout << findValue << "   " << i << endl;
+    //         if (storedArr.find(findValue) != storedArr.end() ) 
+    //             return {i, storedArr[findValue]};
+    //         // if we don't find it , we should add the ele of nums to map
             
-            // store all the ele from vector to the hashmap
-            storedArr[nums[i]] = i;
-        }
-        return {};
+    //         // store all the ele from vector to the hashmap
+    //         storedArr[nums[i]] = i;
+    //     }
+    //     return {};
+    // }
+
+
+
+    // review
+
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     unordered_map <int, int > numMap; 
+
+    //     for (int i = 0; i  < nums.size(); i++ ){
+    //         int complement = target - nums[i];
+
+    //         // find it
+    //         if (numMap.find(complement) != numMap.end()) {
+    //             return {i, numMap[complement]};
+    //         }
+
+    //         // add the nums ele to the numMap for checking later.
+    //         numMap[nums[i]] = i; 
+    //     }
+
+    //     return {};
+
+    // }
+
+    //3rd
+    vector<int> twoSum(vector<int>& nums, int target) {
+       unordered_map <int, int > numMap; 
+       // need index and val
+       for (int i = 0;  i < nums.size(); i++ ) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {i, numMap[complement]};
+            }
+            numMap[nums[i]] = i; 
+       }
+
+       return {};
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
