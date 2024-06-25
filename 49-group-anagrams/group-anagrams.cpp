394c5ee064e3d856fad56867a5dc0762a90acbf3
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-25 08:38:50 -0700

    Time: 63 ms (7.41%) | Memory: 33.1 MB (7.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -27,39 +27,106 @@ public:
     // convert word to counting letters like a token
     // matching token will add words having the same letters in differnt arrangement.  
     // Space complexity: O(N * K)  // Time complexity: O(N * K)
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    //     unordered_map<string, vector<string>> sorted_list;
+    //     vector<vector<string>> result;
+    //     for (auto s : strs) {
+    //         cout << s <<"=========== " << endl;
+    //         string key = getKey(s);
+    //         cout << "key: " << key << endl;
+    //         sorted_list[key].push_back(s);
+    //     }
+
+    //  // since we have a hashmap , key: sortedWord, value: a list of rearranging words
+    //     for (auto & [key, val] : sorted_list ) {
+    //         result.push_back(val);
+    //     }
+
+    //     return result;
+    // }
+
+    // // function to convert the words to couting letters as a token
+    // string getKey (string word) {
+    //     // using vector because of counting how many times a letter appears in a word. 
+    //     vector<int> countLetter(26,0); 
+    //     for (int i = 0; i < word.size(); i++) {
+    //         cout << word[i] << "   " << endl; 
+    //         countLetter[word[i] - 'a']++;
+    //     }
+
+    //     string keyToken = "";
+    //     for (int i = 0; i < countLetter.size(); i++) {
+    //         keyToken += to_string(countLetter[i]) + '#';  
+    //     }
+    //     return keyToken;
+    // }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> sorted_list;
+        //method use: couting letter and map them into a group
+        unordered_map <string, vector<string>> group; 
         vector<vector<string>> result;
-        for (auto s : strs) {
-            cout << s <<"=========== " << endl;
-            string key = getKey(s);
-            cout << "key: " << key << endl;
-            sorted_list[key].push_back(s);
+        for(auto & word: strs) {
+            string key = getKey(word); 
+            cout << "\n " << key << endl; 
+            group[key].push_back(word);
         }
 
-     // since we have a hashmap , key: sortedWord, value: a list of rearranging words
-        for (auto & [key, val] : sorted_list ) {
-            result.push_back(val);
+        for (auto & w: group) {
+            result.push_back(w.second);
         }
-
-        return result;
+        return result;;
     }
 
-    // function to convert the words to couting letters as a token
-    string getKey (string word) {
-        // using vector because of counting how many times a letter appears in a word. 
-        vector<int> countLetter(26,0); 
-        for (int i = 0; i < word.size(); i++) {
-            cout << word[i] << "   " << endl; 
-            countLetter[word[i] - 'a']++;
+    string getKey(string word) {
+        vector<int> count(26);
+        // typically using all the original letters exactly once
+        for (auto & letter: word) {
+            
+            count[letter - 'a']++;
         }
 
-        string keyToken = "";
-        for (int i = 0; i < countLetter.size(); i++) {
-            keyToken += to_string(countLetter[i]) + '#';  
+        string key; 
+        for (auto & c : count) {
+            key += to_string(c) + '#'; 
         }
-        return keyToken;
+
+        return key;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
 };
