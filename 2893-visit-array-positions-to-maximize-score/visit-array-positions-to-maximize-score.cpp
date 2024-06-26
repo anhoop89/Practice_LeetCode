8c04c52118d8510caf134144d25a2866cd6738d7
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-25 20:45:56 -0700

    Time: 274 ms (22.67%) | Memory: 170.8 MB (27.95%) - LeetSync

---
+++ 2893-visit-array-positions-to-maximize-score/visit-array-positions-to-maximize-score.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    using ll = long long;
+    long long maxScore(vector<int>& nums, int x) {
+        int n = nums.size();
+        vector<vector<long long>> memo(n, vector<long long>(2, -1)); 
+        return dp(nums, x, 1, nums[0] & 1, memo) + nums[0];
+    }
+
+    long long dp(vector<int>& nums, int x, int index, int parity, vector<vector<long long>>& memo) {
+        if (index >= nums.size()) {
+            return 0;
+        }
+
+        ll &res = memo[index][parity];
+
+        if (res != -1) return res;
+        
+        ll currParity = nums[index] & 1;
+
+        // Calculate the scores by taking or skipping the current index
+        ll take = nums[index] + dp(nums, x, index + 1, currParity, memo) - (currParity == parity ? 0 : x);
+        ll skip = dp(nums, x, index + 1, parity, memo);
+        return res = max(take, skip); 
+
+
+    }
+};
