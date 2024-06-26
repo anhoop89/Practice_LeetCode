e4e79bff9f37db6a3949f35a9189b3b37cebc04b
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-25 21:36:21 -0700

    Time: 26 ms (67.69%) | Memory: 38.2 MB (69.94%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> res(nums.size());
 
-        //prefix
-        int prefix = 1;
-        for (int i = 0; i < nums.size(); i++ ){
+        int prefix = 1; // left to right
+        for (int i = 0; i < nums.size(); i++){
             res[i] = prefix; 
             prefix *= nums[i];
         }
-        int postfix = 1; 
-        for (int i = nums.size() - 1; i >= 0; i-- ){
-            res[i] *= postfix; 
+
+        int postfix = 1; // right to left
+        for (int i = nums.size() -1; i >= 0 ; i--) {
+            res[i] *= postfix;
             postfix *= nums[i];
         }
 
