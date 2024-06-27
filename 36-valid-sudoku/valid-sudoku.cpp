afd51e8fa7fc2d3957d3484f32b3499993c5d545
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-27 10:21:56 -0700

    Time: 14 ms (75.56%) | Memory: 22.1 MB (74.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        const int cnt = 9;
-        bool row[cnt][cnt] = {false};
-        bool col[cnt][cnt] = {false};
-        bool sub[cnt][cnt] = {false};
-        
-        for(int r = 0; r < cnt; ++r){
-            for(int c = 0; c < cnt; ++c){
-                if(board[r][c] == '.')
-                    continue; // if not number pass
-                
-                int idx = board[r][c] - '0' - 1; //char to num idx
-                int area = (r/3) * 3 + (c/3);
-                
-                //if number already exists
-                if(row[r][idx] || col[c][idx] || sub[area][idx]){
-                    return false;
+       const int n = 9; 
+        bool row[n][n] = {false};
+        bool col[n][n] = {false};
+        bool square[n][n] = {false};
+
+        for (int i = 0; i < n; i++ ){
+            for (int j = 0; j < n; j++){
+                if (board[i][j] != '.') {
+                    int idx = board[i][j]  - '0' - 1;
+                    int area = (i/3) *3 +  (j/3); 
+                    if (row[i][idx] || col[j][idx] || square[area][idx])
+                        return false;
+
+                         row[i][idx] = true;
+                col[j][idx] = true;
+                square[area][idx] = true;
                 }
-                
-                row[r][idx] = true;
-                col[c][idx] = true;
-                sub[area][idx] = true;
             }
-        }
+        }    
         return true;
     }
 };
