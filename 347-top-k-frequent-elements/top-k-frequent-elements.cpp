dd50049e2f2e96839ed41952c0f8cf8a2efae539
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-26 22:19:05 -0700

    Time: 12 ms (62.45%) | Memory: 19 MB (7.34%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -34,39 +34,76 @@ public:
 
 
 
-     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> count; 
-        for (auto & val: nums) {
-            count[val]++;
+    //  vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int, int> count; 
+    //     for (auto & val: nums) {
+    //         count[val]++;
+    //     }
+
+    //     vector<vector<int>>store(nums.size()+1);
+    //     for(auto & [key, val]: count) {
+    //         store[val].push_back(key);    
+    //     }
+
+    //     vector<int> result;
+    //     for(int i = nums.size(); i > 0 & k >0; i--){
+    //         for(auto & val : store[i]) {
+    //             // it wont loop inside of each ele
+    //             // just get the whole part and insert into the result
+    //             result.push_back(val);
+    //             if (--k == 0) break;
+    //         }
+    //     }
+    //     return result;
+
+    //  }
+
+
+
+
+
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> mp; 
+        for (auto & val : nums) {
+            mp[val]++;
         }
 
-        vector<vector<int>>store(nums.size()+1);
-        for(auto & [key, val]: count) {
-            store[val].push_back(key);    
+        // key is number 
+        // val is count
+
+        // store 
+        //count    1 2 3 4 5 6 
+        //num      6     3   1 
+        vector<vector<int>> store(nums.size() + 1);
+        for (auto & [key, val]: mp) {
+            store[val].push_back(key);
         }
 
-        vector<int> result;
-        for(int i = nums.size(); i > 0 & k >0; i--){
-            for(auto & val : store[i]) {
-                // it wont loop inside of each ele
-                // just get the whole part and insert into the result
-                result.push_back(val);
-                if (--k == 0) break;
+        vector<int> res; 
+        for(int i = nums.size(); i > 0 & k > 0 ; i--) {
+            for (auto & val: store[i]) // more than one 
+            {
+                res.push_back(val);
+                if (--k == 0) return res;
             }
         }
-        return result;
-
-     }
+        return res;
 
 
-
+    }
 
 
 
 
+//  hashtable: 
 
-
-
+//  nums = [1,1,1,2,2,3]
+//  key: val
+//  1: 3
+//  2: 2
+//  3: 1
 
 
 
