f42bdfe45bc45c2cb13a96c153949a561d6f95b7
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-06-27 10:29:10 -0700

    Time: 18 ms (51.15%) | Memory: 23.8 MB (51.18%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -12,7 +12,7 @@ public:
         for (int i = 0; i < n; i++) { // Iterate over each row
             for (int j = 0; j < n; j++) { // Iterate over each column
                 if (board[i][j] != '.') { // If the cell is not empty
-                    int idx = board[i][j] - '0' - 1; // Convert the char digit to an index (0-8)
+                    int idx = board[i][j] - '1'; // Convert the char digit to an index (0-8)
                     int area = (i / 3) * 3 + (j / 3); // Calculate the index for the 3x3 sub-grid
 
                     // Check if the number already exists in the current row, column, or 3x3 sub-grid
