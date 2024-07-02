304b47445bc5c3bf01c236e2d4592a2b429c5aa5
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-07-01 18:04:46 -0700

    Time: 65 ms (5.09%) | Memory: 8.5 MB (83.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if (s.empty()) return true;
+        
         int i = 0;
-        int j = s.length() - 1 ;
+        int j = s.length() - 1;
+        while (i < j ){
+            cout << "i " << s[i] << "  -- j: " << s[j] << endl;
+            if (!isalnum(s[i])) { ++i; continue;}
+            if (!isalnum(s[j])) { --j; continue;}
 
-        while ( i <= j) {
-            cout << s[i] << " - " << i << " --- " << s[j] << " - " << j << endl;
-            if (!isalnum(s[i])) {
-                ++i;
-                continue;
-            }
-
-            if (!isalnum(s[j])) {
-                --j;
-                continue;
-            }
-
-            if (i < j && tolower(s[i]) != tolower(s[j]))
+            if (tolower(s[i]) != tolower(s[j]))
                 return false;
-
+            
             ++i;
             --j;
         }
-
-    
         return true;
-    }
-
-    bool check(char c) {
-        if (tolower(c) >= 'a' && tolower(c) <= 'z') {
-            return true;
-        }
-        return false;
     }
 };
