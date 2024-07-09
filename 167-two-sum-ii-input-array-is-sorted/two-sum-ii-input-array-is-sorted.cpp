20ed6e3c5dd0b9e22cbef4e035cb5981bcc2fb06
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-07-08 23:41:00 -0700

    Time: 11 ms (36.88%) | Memory: 17.8 MB (99.52%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0; 
-        int j = numbers.size() - 1; 
-
-        while ( i < j) {
-            if ((numbers[i] + numbers[j]) > target) 
-                j--; 
-            else if ((numbers[i] + numbers[j]) < target) 
-                i++;
-            
-            if ((numbers[i] + numbers[j]) == target)
-                return {i+1, j+1}; 
+    vector<int> twoSum(vector<int>& n, int target) {
+        int size = n.size() - 1;
+        int i = 0;
+        int j = size;
+        // 2 pointers, running both side to check the sum
+        while (i < j) {
+            int check = n[i] + n[j];
+            if ( check > target)
+                --j;
+            if ( check < target)
+                ++i;
+            if (check == target)
+                return {i+1, j+1};
         }
-
         return {};
     }
 };
