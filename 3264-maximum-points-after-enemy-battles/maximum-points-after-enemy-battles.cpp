f9ef4ac96dd15fbd2a0d6e854e7e02a4e82a5e39
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-07-08 17:52:15 -0700

    Time: 101 ms (92.10%) | Memory: 75.1 MB (91.43%) - LeetSync

--- 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     // this approach: nlogn
+    // long long maximumPoints(vector<int>& e, int c) {
+    //     // e = enemyEnergies, c = currentEnergy; 
+    //     // sorting to find the minimum 
+    //     long long p = 0;
+    //     sort(e.begin(), e.end());
+    //     int first = e[0]; 
+    //     if (c < first) return 0;
+    //     for (int i = e.size(); i >0; ){
+    //        if (c >= first) { 
+    //             p += c / first; // total point
+    //             c = c % first; // remainder
+    //         } else 
+    //             c += e[--i];
+    //     }
+    //     return p;
+    // }
+
+
+    // another approach 
     long long maximumPoints(vector<int>& e, int c) {
-        // e = enemyEnergies, c = currentEnergy; 
-        // sorting to find the minimum 
-        long long p = 0;
-        sort(e.begin(), e.end());
-        int first = e[0]; 
-        if (c < first) return 0;
-        for (int i = e.size(); i >0; ){
-           if (c >= first) { 
-                p += c / first; // total point
-                c = c % first; // remainder
-            } else 
-                c += e[--i];
-        }
-        return p;
+        auto min 
+        = min_element(e.begin(), e.end()); 
+        long long sum = 0;
+        if (c < *min) return 0;
+        for (auto & v : e)
+            sum += v;
+        cout << "sum : " << sum << " min: " << *min <<endl;
+        return ((sum + c)/ *min) - 1 ;
+
     }
 };            
