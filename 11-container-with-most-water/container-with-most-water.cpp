25dcb498f6532a1c6f05630a76e0351c3c44c12f
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-07-10 23:41:31 -0700

    Time: 334 ms (5.04%) | Memory: 61.5 MB (27.04%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int l = 0; 
+        int r = h.size() - 1;
+        int res = -111111;
+        while ( l < r) {
+            int temp = (r - l ) * min(h[l],h[r]);
+            cout << "temp: " <<temp << min(h[l],h[r])<<  endl;
+            res = max(res, temp);
+            if (h[l] >= h[r]) 
+                --r; 
+            else 
+                ++l;
+        }
+        return res;
+    }
+};
