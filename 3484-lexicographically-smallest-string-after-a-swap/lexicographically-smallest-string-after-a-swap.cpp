a398e26773bdef739d86950b60917fe142416d14
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-07-14 22:54:43 -0700

    Time: 5 ms (66.67%) | Memory: 8 MB (83.33%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    string getSmallestString(string s) {
-        string res = s; 
+    string getSmallestString(string s) { 
         for (int i = 1; i < s.length(); i++){
-            if ((s[i-1] - '0') % 2 == (s[i] - '0') % 2) {
+            if (((s[i-1] - '0') & 1) == ((s[i] - '0') & 1) && s[i-1] > s[i]) {
                 swap(s[i-1],s[i]);
-                if (s < res) 
-                    return s;
-                s = res;
+                break;                
             }
         }
         return s;
