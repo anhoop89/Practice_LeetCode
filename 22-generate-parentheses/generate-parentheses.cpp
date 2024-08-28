ee78c15d531d18a7b4f13fa018e40a413aa05a00
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2024-08-28 13:31:03 -0700

    Time: 0 ms (100.00%) | Memory: 13.1 MB (76.88%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -5,7 +5,7 @@ public:
         // need a var to save result
         vector<string> res;
         // defind the function
-        function<void(int, int, string)> result = [&](int open, int close, string update) {
+        function<void(int, int, string)> backTracking = [&](int open, int close, string update) {
             // if open == close == n 
             if (open == n && open == close) {
                 res.push_back(update);
@@ -13,13 +13,13 @@ public:
             }
 
             if (open < n) 
-                result(open + 1, close, update + "(");
+                backTracking(open + 1, close, update + "(");
             
             if (close < open)
-                result(open, close + 1, update + ")");
+                backTracking(open, close + 1, update + ")");
         };
 
-        result(0,0,"");
+        backTracking(0,0,"");
 
         return res;
     }
