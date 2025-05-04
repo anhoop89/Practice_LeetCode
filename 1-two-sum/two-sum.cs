0f3ac2f2d25ebd783eeb4a2ca7ee022fc7cc778b
if (Anh) true <102849461+anhoop89@users.noreply.github.com>
2025-05-04 13:36:47 -0700

    Time: 1 ms (98.50%) | Memory: 49.1 MB (24.76%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,13 +1,18 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        int s = nums.Length;
-        for (int i = 0; i < s; i++) {
-            for (int j = i + 1; j < s; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
+       Dictionary <int,int> map = new Dictionary<int, int>(); 
+
+        for (int i = 0; i < nums.Length; i++) {
+            int minusNum = target - nums[i];
+            if (map.ContainsKey(minusNum)) {
+                return [map[minusNum],i];
+            } 
+
+                map.TryAdd(nums[i],i);
         }
-        return new int[0]; // or throw an exception
+
+        return new int[0];
     }
 }
+
+// hashtable - add first ele. 2 will
